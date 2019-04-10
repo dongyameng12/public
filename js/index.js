458ae5c6f3ea2483b71543793f8d0f046d6dd323
yangtao <1509440167@qq.com>
2019-04-10 18:54:23 +0800

    1

--- js/index.js
+++ js/index.js
@@ -4,53 +4,78 @@
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            docEl.style.fontSize = 100 * (clientWidth /750) + 'px';
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })
-    (document, window);
-
+(document, window);
 $(document).ready(function () {
-    // 成功或失败
-    var isfail = true;
-    // 点击ios展开
-    $('#ios_btn').on('click',function(){
-        $('.ios_zhankai').toggle();
-        var displayStyle = $('.ios_zhankai').css('display'); 
-       if (displayStyle === 'block') {
-         $('#jian_xuan').addClass('xuanzhuan');
-       } else {
-        $('#jian_xuan').removeClass('xuanzhuan');
-       }
-    })
-    // 点击android_btn展开
-    $('#android_btn').on('click',function(){
-        $('.android_zhankai').toggle();
-        var displayStyle = $('.android_zhankai').css('display'); 
-       if (displayStyle === 'block') {
-         $('#ajian_xuan').addClass('xuanzhuan');
-       } else {
-        $('#ajian_xuan').removeClass('xuanzhuan');
-       }
+    var swiper1 = new Swiper('#swiper1',{
+        observer:true,
+        observeParents:true,
+        loop: true, // 循环模式选项
+        autoplay: true,//可选选项，自动滑动
+        pagination: {
+            el: '.pagination1',
+        },
     });
-    // 点击开通
-    $('#deal').on('click',function(){
-        showMask();
-        $('.progress').show();
-        setTimeout(function(){
-            $('.progress').hide();
-            isfail?$('.success').show():$('.fail').show();
-        },2000)
-
+    var swiper2 = new Swiper('#swiper2',{
+        init: false,
+        observer:true,
+        observeParents:true,
+        loop: true, // 循环模式选项
+        autoplay: true,//可选选项，自动滑动
+        pagination: {
+            el: '.pagination2',
+        },
     });
-    // 点击遮罩
-    $('#mask').on('click',function(){
-        hideMask();
-        $('.tc').hide();
-    })
+    var swiper3 = new Swiper('#swiper3',{
+        init: false,
+        observer:true,
+        observeParents:true,
+        loop: true, // 循环模式选项
+        autoplay: true,//可选选项，自动滑动
+        pagination: {
+            el: '.pagination3',
+        },
+    });
+    // swiper1.init()
+   $('#tab p').on('click',function(){
+    var className = $(this).children('span:eq(1)');
+    if(!className.hasClass('act')){
+        var p_abject = $(this).siblings('p');
+        $.each(p_abject,function(index,item){
+            if( $(item).children('span:eq(1)').hasClass('act')){
+                $(item).children('span:eq(1)').removeClass('act');
+            }
+        });
+        $(this).children('span:eq(1)').addClass('act');
+        // console.log($(this).index()) 判断点击的是第几个p
+        if ($(this).index() == 0) {
+            show_lunbo(0)
+            swiper1.init()
+        } else if ($(this).index() == 1) {
+            show_lunbo(1)
+            swiper2.init()
+        } else if ($(this).index() == 2) {
+            show_lunbo(2)
+            swiper3.init()
+        }
+    } else {
+        
+    }
+   }); 
+   
 });
+// 展示的是哪个轮播图
+function show_lunbo (index) {
+    if (!$('.bg:eq('+index+')').hasClass('bg_active')) {
+        $('.bg:eq('+index+')').siblings('.bg').removeClass('bg_active')
+        $('.bg:eq('+index+')').addClass('bg_active')
+    }
+}
 //显示遮罩层
 function showMask() {
     $("#mask").css("height", $(document).height());
