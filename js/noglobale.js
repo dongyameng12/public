9da03418d35340f1b8646fefbff4bb9fd7adf612
yangtao <1509440167@qq.com>
2019-04-11 18:59:25 +0800

    qq

--- js/noglobale.js
+++ js/noglobale.js
@@ -1,4 +1,4 @@
-﻿(function (doc, win) {
+(function (doc, win) {
     var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
