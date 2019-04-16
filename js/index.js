6bf07438e65d734c5a7f815310bd440a6ecb05db
yangtao <1509440167@qq.com>
2019-04-16 11:22:23 +0800

    qqq

--- js/index.js
+++ js/index.js
@@ -13,7 +13,28 @@
     (document, window);
 
 $(document).ready(function () {
-  
+    // 首页翻书
+    (function () {
+        var bgCounter = 0,
+            backgrounds = [
+                "images/homex_01.png",
+                "images/homex_02.png",
+                "images/homex_03.png",
+                "images/homex_04.png",
+            ];
+        function changeBackground() {
+            bgCounter = (bgCounter + 1) % backgrounds.length;
+            $('#change').css('background', 'url(' + backgrounds[bgCounter] + ') no-repeat ');
+            $('#change').css('background-size', 'cover');
+            setTimeout(changeBackground, 510);
+        }
+        changeBackground();
+    })();
+    // 开启回忆
+    $('#start_memories').on('click',function(){
+       alert(1)
+    });
+
 });
 //显示遮罩层
 function showMask() {
